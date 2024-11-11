924ff3bbfbd1ae797d7fd181d6b34efa3f855c67
Era Rexhepi <era.rexhepi11@gmail.com>
2024-11-11 13:26:11 +0200

    Resolved conflict between main and bugikorjaus branches

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -4,21 +4,16 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-<<<<<<< HEAD
 logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
-print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
-=======
-logger("aloitetaan ohjelma")  # yhdistetty muutos
 
-x = int(input("luku 1: "))
-y = int(input("luku 2: "))
+# Molempien haarojen tulostukset yhdistetty
 print(f"{x} + {y} = {summa(x, y)}")  # muutos mainissa
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
 print(f"{x} - {y} = {erotus(x, y)}")  # muutos mainissa
->>>>>>> main
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
 
 logger("lopetetaan ohjelma")
 print("goodbye!")
