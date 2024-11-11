f1669fe1d6e548fe557318faff94fc6c5ca54d89
Era Rexhepi <era.rexhepi11@gmail.com>
2024-11-11 13:17:51 +0200

    fixed some branching mistakes

--- viikko2/git-branch-harjoitus/logger.py
+++ viikko2/git-branch-harjoitus/logger.py
@@ -1,12 +1,8 @@
 from logger import logger
-from summa import summa
-from erotus import erotus
 
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
 
 logger("lopetetaan")
