143a2f1742dfa5f813d8405b0d6e916704e191db
Era Rexhepi <era.rexhepi11@gmail.com>
2024-12-09 19:29:38 +0200

    ex3week6

--- viikko6/src/index.py
+++ viikko6/src/index.py
@@ -1,39 +1,31 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, Or, HasAtLeast, PlaysIn
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    print("Pelaajat, joilla on vähintään 5 maalia, vähintään 20 syöttöä ja jotka pelaavat PHI-joukkueessa:")
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
-
-    for player in stats.matches(matcher):
-        print(player)
-
-    print("\nKaikki pelaajat (All):")
-    all_players = stats.matches(All())
-    print(f"Kokonaismäärä: {len(all_players)} pelaajaa")
-
-    print("\nNYR-pelaajat, joilla on alle 2 maalia (Not + HasAtLeast):")
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
+    print("Pelaajat, joilla on vähintään 45 maalia tai 70 syöttöä:")
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
     )
     for player in stats.matches(matcher):
         print(player)
 
-    print("\nPelaajat, joilla on alle 5 maalia (HasFewerThan):")
-    matcher = HasFewerThan(5, "goals")
+    print("\nPelaajat, joilla on vähintään 70 pistettä ja jotka pelaavat NYR-, FLA- tai BOS-joukkueissa:")
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
